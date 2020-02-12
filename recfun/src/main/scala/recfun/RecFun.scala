dd9a3b07c7df065c79ada2ae46630481ce23a66f
robinovitch61 <leorobinovitch@gmail.com>
2020-02-12 15:33:44 -0800

    add better algorithms

--- recfun/src/main/scala/recfun/RecFun.scala
+++ recfun/src/main/scala/recfun/RecFun.scala
@@ -4,7 +4,7 @@ object RecFun extends RecFunInterface {
 
   def main(args: Array[String]): Unit = {
     println("Pascal's Triangle")
-    for (row <- 0 to 10) {
+    for (row <- 0 to 20) {
       for (col <- 0 to row)
         print(s"${pascal(col, row)} ")
       println()
@@ -17,33 +17,22 @@ object RecFun extends RecFunInterface {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-    if (c == 0) 1 else {
-      if (r == 0) 0 else
-        pascal(c, r - 1) + pascal(c - 1, r - 1)
-    }
+    if (c == 0 || r == c) 1
+    else pascal(c, r - 1) + pascal(c - 1, r - 1)
   }
 
   /**
    * Exercise 2
    */
-//  def balance(chars: List[Char]): Boolean = {
-//    def remove(idx: Int, list: List[Char]) =
-//      list diff List(list.apply(idx))
-//
-//    val idxOpen = chars.indexOf('(')
-//    val idxClose = chars.indexOf(')')
-//    println(chars.toString, idxOpen, idxClose)
-//    if (idxOpen == -1 && idxClose == -1) true else {
-//      if (idxOpen == -1 || idxClose == -1) false else {
-//        val newList = remove(idxOpen, chars)
-//        val finalList = remove(List(0, idxClose - 1).max, newList)
-//        if (finalList.length == 0) true else balance(finalList)
-//      }
-//    }
-//  }
-def balance(chars: List[Char]): Boolean = {
-  true
-}
+  def balance(chars: List[Char]): Boolean = {
+    def isBalanced(chars: List[Char], count: Int): Int = {
+      if (chars.isEmpty || count < 0) count
+      else if (chars.head == '(') isBalanced(chars.tail, count + 1)
+      else if (chars.head == ')') isBalanced(chars.tail, count - 1)
+      else isBalanced(chars.tail, count)
+    }
+    isBalanced(chars, 0) == 0
+  }
 
   /**
    * Exercise 3
